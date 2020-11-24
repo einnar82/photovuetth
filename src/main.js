80765958f1683de5e50df538ae84062ec2e3be4e
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-11-24 16:12:31 +0800

    WIP

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import Cloudinary, { CldImage, CldTransformation } from "cloudinary-vue";
 
 Vue.use(Buefy)
+Vue.use(Cloudinary, {
+  configuration: { cloudName: "demo" },
+  components: {
+    CldImage,
+    CldTransformation
+  }
+});
 Vue.config.productionTip = false
 
 new Vue({
